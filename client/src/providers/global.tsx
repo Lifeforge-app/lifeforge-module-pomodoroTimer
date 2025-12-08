f48831f9a83000b32510b387d7510fde80168ee1
Melvin Chia <melvinchia623600@gmail.com>
2025-12-08 17:28:06 +0800

    feat(client): add authentication check in PomodoroProviders

--- client/src/providers/global.tsx
+++ client/src/providers/global.tsx
@@ -1,9 +1,17 @@
+import { useAuth } from 'shared'
+
 import ActiveSessionProvider from './ActiveSessionProvider'
 import CurrentSessionProvider from './CurrentSessionProvider'
 import PomodoroProvider from './PomodoroProvider'
 import PomodoroSettingsProvider from './PomodoroSettingsProvider'
 
 function PomodoroProviders({ children }: { children: React.ReactNode }) {
+  const { auth } = useAuth()
+
+  if (!auth) {
+    return children
+  }
+
   return (
     <ActiveSessionProvider>
       <PomodoroSettingsProvider>
