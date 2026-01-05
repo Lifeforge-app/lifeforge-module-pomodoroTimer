cdbc49c92d181b4ae9a12f871530d7e2702d6b57
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:00:14 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,11 +2,8 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Pomodoro Timer',
-  icon: 'tabler:clock-bolt',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Productivity',
   clearQueryOnUnmount: false
 } satisfies ModuleConfig
