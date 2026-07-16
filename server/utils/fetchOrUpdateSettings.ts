afcb42102b7e79bb62300987d823c3f8ba9e7d73
melvinchia3636 <melvinchia623600@gmail.com>
2026-07-16 22:18:42 +0800

    feat: update to match new SDK

--- server/utils/fetchOrUpdateSettings.ts
+++ server/utils/fetchOrUpdateSettings.ts
@@ -4,7 +4,7 @@ import z from 'zod'
 
 import pomodoroTimerSchemas from '../schema'
 
-const DEFAULT_SOUND_LOCATION = path.resolve(__dirname, '../assets/bell.opus')
+const DEFAULT_SOUND_LOCATION = path.resolve(import.meta.dirname, '../assets/bell.opus')
 
 const DEFAULT_SETTINGS: Omit<
   z.infer<typeof pomodoroTimerSchemas.settings>,
