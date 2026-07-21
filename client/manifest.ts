bf4b5d7e9ae861d56bbc25933f3f7462766f12b1
melvinchia3636 <melvinchia623600@gmail.com>
2026-07-21 21:25:23 +0800

    refactor: update to match new SDK

--- client/manifest.ts
+++ client/manifest.ts
@@ -1,10 +1,10 @@
 import { lazy } from 'react'
 
-import { createForgeModuleClient } from '@lifeforge/federation'
+import { createForgeModule } from '@lifeforge/federation'
 
 import contract from './contract'
 
-const { forgeAPI, ...manifest } = createForgeModuleClient({
+const { forgeAPI, ...manifest } = createForgeModule({
   provider: lazy(() => import('@/providers/PomodoroProviders')),
   routes: {
     '/': lazy(() => import('@'))
